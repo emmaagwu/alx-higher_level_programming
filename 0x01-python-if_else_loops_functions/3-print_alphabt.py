377232f21a4adc784f78e51b5cf530ab0a50fa20
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-11-28 16:28:08 -0800

    modified code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(97, 123):
     if chr(char) != 'e' and chr(char) != 'q':
-        print("{}".formatt(chr(char)), end="")
+        print("{}".format(chr(char)), end="")
