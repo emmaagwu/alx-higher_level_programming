72041753ace49118efa458559f12126b627217fd
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-12-04 14:59:33 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    list_length = len(my_list)
-    if idx < 0 or idx > list_length:
-        return "None"
-    return my_list[idx]
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
