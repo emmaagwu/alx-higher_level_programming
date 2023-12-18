5f893af4ce5bccaac6804940b00012134a81f86a
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-12-18 03:23:19 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,6 @@ def safe_print_list(my_list=[], x=0):
     """ Prints x elements of a list.
     """
     i = 0
-    if x == 0:
-        return (0)
     while i < x:
         try:
             print("{}".format(my_list[i]), end="")
