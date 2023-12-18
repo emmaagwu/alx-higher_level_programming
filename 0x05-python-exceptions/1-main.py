cdb6d2e2036dde55bdfd2b112a92ffb432b76115
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-12-18 03:49:16 -0800

    task 0

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -9,7 +9,7 @@ if not has_been_print:
 value = -89
 has_been_print = safe_print_integer(value)
 if not has_been_print:
-    print("{} is not an integer".format(value))
+    print("{} is not n integer".format(value))
 
 value = "School"
 has_been_print = safe_print_integer(value)
