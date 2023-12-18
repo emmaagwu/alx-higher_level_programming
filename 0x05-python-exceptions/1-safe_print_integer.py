e3497a1738142fad754eb5308321cdca642a0792
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-12-18 03:58:04 -0800

    task 0

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return ("true")
+        return (True)
     except ValueError:
-        return ("false")
+        return (False)
