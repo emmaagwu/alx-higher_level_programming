773b562a6ab5adec903f23abb38b8272e2a834e0
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-12-19 05:55:29 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" Here I defined the class by the private attribute size """
+
+
+class Square:
+    """Defines a square class"""
+
+
+    def __init__(self, size):
+        """ initialize a new square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
