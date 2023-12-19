8b4f2ba21bba8d082032c07cc5af0b0c2bad1ac9
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-12-19 12:26:23 -0800

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# 5-square.py
+
+"""Defines a square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initializies the square
+
+        Args:
+            size(int): The size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """gets the current value of size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets the new value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+            return (self.__size)
+
+    def area(self):
+        """Returns the area of square"""
+        return (self.__size ** 2)
+
+    
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
