cc06168702bc5f747b9d8b1bbcbec18c4c9695b7
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-01-09 04:30:56 -0800

    task 3

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+""" defines a function that returns an object from JSON """
+import json
+
+
+def from_json_string(my_str):
+    """ Returns an object from a JSON string """
+    return json.loads(my_str)
