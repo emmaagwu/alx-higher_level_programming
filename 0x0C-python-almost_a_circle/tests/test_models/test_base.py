499f62fb88f8adf3d7486ca09199724397cde266
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-01-15 20:55:42 -0800

    fix test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ defines test cases for the base class """
+import os
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestBaseClass(unittest.Testcase):
+class TestBaseClass(unittest.TestCase):
     """ Defines test cases for the base class """
 
     def test_with_id(self):
